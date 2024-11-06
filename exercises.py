0a8dd9c8b45dac043b06d62d6b919c248b63ad5c
DrueWolf <dwolf0305@gmail.com>
2024-11-05 20:25:45 -0500

    exercie 1

---
+++ exercises.py
@@ -0,0 +1,14 @@
+# Exercise 1: Calculate Area of a Triangle
+#
+# Write a function named `calculate_area_triangle` that takes the base and height of a triangle and returns the area.
+# The area formula is (base * height) / 2.
+#
+# Examples:
+# calculate_area_triangle(10, 5) should return 25.0.
+# calculate_area_triangle(7, 3) should return 10.5.
+#
+# Define your function and call it below.
+def calculate_area_triangle(a, b):
+    return (a * b) / 2
+
+print('Exercise 1:', calculate_area_triangle(10, 5))
